3dae868c03109b6d27651b1f6d4b39de87ba15f6
Aakash <aakashjha343@gmail.com>
2024-06-17 23:35:43 +0530

    recursion fact and fibo done

--- Recursion/factorial.py
+++ Recursion/factorial.py
@@ -1,19 +1,47 @@
-def fact(n):
+def fact_recursive(n):
     if n == 0 or n == 1:
         return 1
     else:
-        return n * fact(n-1)
+        return n * fact_recursive(n - 1)
 
 
-try:
-    num = float(input("Enter a whole number to finds its factorial : "))
-    if num >= 0:
-        print(f"The factorial of {num} is {fact(num)}")
-    else:
-        print("Invalid input try again")
-except Exception as e:
-    print(f"Oops an error occured !\n{str(e)} \nTry again ")
-finally:
-    print("\nDhanyavad ji , fhir milengee ....")
-    print("Created by")
-    print("Aakash Jha")
+def fact_iterative(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+
+def main():
+    while True:
+        print("\nMenu:")
+        print("1. Calculate Factorial (Recursive)")
+        print("2. Calculate Factorial (Iterative)")
+        print("3. Exit")
+
+        choice = input("Enter your choice: ")
+        while choice not in ('1', '2', '3'):
+            print("Invalid choice. Please try again.")
+            choice = input("Enter your choice: ")
+        print("")
+
+        choice = int(choice)
+        if choice == 1:
+            num = float(input("Enter a whole number to find its factorial: "))
+            print(f"The factorial of {num} (recursive) is {
+                fact_recursive(num)}")
+        elif choice == 2:
+            num = int(input("Enter a whole number to find its factorial: "))
+            print(f"The factorial of {num} (iterative) is {
+                fact_iterative(num)}")
+        elif choice == 3:
+            print("Exiting the program. Dhanyavad ji, phir milenge...")
+            print("Created by")
+            print("Aakash Jha")
+            break
+        else:
+            print("Invalid input. Please enter a non-negative whole number.")
+
+
+if __name__ == "__main__":
+    main()
