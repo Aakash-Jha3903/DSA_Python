226f617b81b98f7372bd562f01d6a39d452a170c
Aakash <aakashjha343@gmail.com>
2024-07-01 23:38:33 +0530

    Round Robin algorithm

---
+++ Operating_System_Algorithms/Round_Robin_algo.py
@@ -0,0 +1,46 @@
+def round_robin(processes, burst_times, time_slice):
+    n = len(processes) 
+    remaining_burst_times = burst_times[:]  # Copy of burst times
+    waiting_times = [0] * n
+    turnaround_times = [0] * n
+    total_waiting_time = 0
+    total_turnaround_time = 0
+    
+    current_time = 0  # Current time in the CPU schedule
+    while True:
+        done = True
+        for i in range(n):
+            if remaining_burst_times[i] > 0:
+                done = False
+                if remaining_burst_times[i] > time_slice:
+                    current_time += time_slice
+                    remaining_burst_times[i] -= time_slice
+                else:
+                    current_time += remaining_burst_times[i]
+                    waiting_times[i] = current_time - burst_times[i]
+                    remaining_burst_times[i] = 0
+
+        if done:
+            break
+
+    for i in range(n):
+        turnaround_times[i] = burst_times[i] + waiting_times[i]
+        total_waiting_time += waiting_times[i]
+        total_turnaround_time += turnaround_times[i]
+
+    average_waiting_time = total_waiting_time / n
+    average_turnaround_time = total_turnaround_time / n
+
+    print(f"Processes    Burst Time    Waiting Time    Turnaround Time")
+    for i in range(n):
+        print(f"    {processes[i]}           {burst_times[i]}              {waiting_times[i]}               {turnaround_times[i]}")
+
+    print(f"\nAverage Waiting Time: {average_waiting_time}")
+    print(f"Average Turnaround Time: {average_turnaround_time}")
+
+# Example run
+processes = [1, 2, 3, 4]
+burst_times = [5, 4, 3, 2]
+time_slice = 2
+
+round_robin(processes, burst_times, time_slice)
--- Operating_System_Algorithms/Shortest_Job_First (SJF)_Algo.py
+++ Operating_System_Algorithms/Shortest_Job_First (SJF)_Algo.py
@@ -6,31 +6,25 @@ def SJF_Scheduling(processes):
     # Sort processes based on burst time (shortest job first)
     processes.sort(key=lambda x: x[1])
 
-    # Calculate waiting time and turnaround time
     waiting_time = [0] * n
     turnaround_time = [0] * n
 
     # First process has 0 waiting time
     waiting_time[0] = 0
 
-    # Calculate waiting time for each process
     for i in range(1, n):
         waiting_time[i] = waiting_time[i - 1] + processes[i - 1][1]
 
-    # Calculate turnaround time for each process
     for i in range(n):
         turnaround_time[i] = waiting_time[i] + processes[i][1]
 
-    # Calculate total waiting time and total turnaround time
     for i in range(n):
         total_waiting_time += waiting_time[i]
         total_turnaround_time += turnaround_time[i]
 
-    # Calculate averages
     average_waiting_time = total_waiting_time / n
     average_turnaround_time = total_turnaround_time / n
 
-    # Display results
     print("Process\t Burst Time\t Waiting Time\t Turnaround Time")
     for i in range(n):
         print(f"P{i}\t\t {processes[i][1]}\t\t {waiting_time[i]}\t\t {turnaround_time[i]}")
