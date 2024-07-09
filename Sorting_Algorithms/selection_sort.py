7d37edda72071d4da6ec2d78256ba4bff425cafc
Aakash <aakashjha343@gmail.com>
2024-07-09 19:19:04 +0530

    Bubble Sort

--- Sorting_Algorithms/selection_sort.py
+++ Sorting_Algorithms/selection_sort.py
@@ -1,17 +1,14 @@
-# Python program for implementation of Selection
-# Sort
-A = [64, 25, 12, 22, 11]
+# implementation of Selection Sort
+arr = [64, 25, 12, 22, 11]
 
 # Traverse through all array elements
-for i in range(len(A)-1):    
+for i in range(len(arr)-1):    
     min_idx = i
-    for j in range(i+1, len(A)):
-        if A[min_idx] > A[j]:
+    for j in range(i+1, len(arr)):
+        if arr[min_idx] > arr[j]:
             min_idx = j
             
     # Swap the found minimum element with the "first" element        
-    A[i], A[min_idx] = A[min_idx], A[i]
+    arr[i], arr[min_idx] = arr[min_idx], arr[i]
 
-print ("This is your Sorted array")
-for i in range(len(A)):
-    print(A[i],end=" ")
+print("This is your Sorted array : ",arr)
